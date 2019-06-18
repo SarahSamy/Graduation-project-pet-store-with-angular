408114e823a964687ac578c6bc64c2f2e8eb52c5
samar12 <samarAboHabib@gmail.com>
2019-06-18 20:42:01 +0200

    blog-heirarchy

---
+++ src/app/features/blog/tags/tag-item/tag-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TagItemComponent } from './tag-item.component';
+
+describe('TagItemComponent', () => {
+  let component: TagItemComponent;
+  let fixture: ComponentFixture<TagItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TagItemComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TagItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

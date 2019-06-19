cbfceeb5ed1d17c5ac93906a8d04832a5bffe1d4
sarah <sarah.refaat92@gmail.com>
2019-06-19 10:44:53 +0200

    edit filter in pet listing

--- src/app/features/pets/pet-listing/pet-listing.component.ts
+++ src/app/features/pets/pet-listing/pet-listing.component.ts
@@ -11,47 +11,94 @@ import { TypeService } from 'src/app/_services/type.service';
 export class PetListingComponent implements OnInit {
   @Input() data: Pet[]
   @Input() mostViewed: Pet[]
-  // id:Number
-  
-  constructor(private petService: PetService) { }
+  page : number;
+  pageSize :number;
+  collectionSize :number;
+  showed:number;
+
+  constructor(private petService: PetService) { 
+    this.page=1;
+    this.pageSize=4;
+    this.collectionSize=9;
+    this.showed=4;
+  }
 
   ngOnInit() {
-    if(!this.data){
-      this.data= this.petService.getAll().filter(p=>p.category.type.id<4);
+
+    
+    if (!this.data) {
+      this.data = this.petService.getAll()
+      // .filter(p => p.category.type.id < 4);
     }
-    if(!this.mostViewed){
-      this.mostViewed=this.petService.getMostViews();
+    if (!this.mostViewed) {
+      this.mostViewed = this.petService.getMostViews();
+    }
+
+  }
+
+
+  getAllTypes(){
+    this.data = this.petService.getAll();
+      return  this.data;
+  }
+
+  getByPetType(typeId:number){
+    this.data = this.petService.getByType(typeId);
+    
+      return  this.data;
+  }
+  getOthers() {
+    this.data = this.petService.getAllOthers();
+    return this.data;
+  }
+
+
+ 
+  getMostViews() {
+    this.mostViewed = this.petService.getMostViews();
+    return this.mostViewed;
+  }
+
+
+
+  nextPage(){
+    if (this.showed>0){
+      this.page++;
+      this.showed= this.collectionSize-this.pageSize;
     }
     
   }
-  getCats(){
-    this.data=this.petService.getAllCats();
-    // alert("cattttttt");
-    return this.petService.getAllCats();
+
+  myalert(text: string) {
+    alert(text);
   }
-  getDogs(){
-    this.data=this.petService.getAllDogs();
-    return this.petService.getAllDogs();
-  }
-  getBirds(){
-    this.data=this.petService.getAllBirds();
-    return this.petService.getAllBirds();
-  }
-  getOthers(){
-    this.data=this.petService.getAllOthers();
-    return this.petService.getAllOthers();
-  }
-  getAll(){
-    this.data= this.petService.getAll();
-    return this.petService.getAll();
-  }
-  getMostViews(){
-    this.mostViewed=this.petService.getMostViews();
-    return this.mostViewed;
-  }
-myalert(text:string){
-  alert(text);
-}
+
+
+ //-----------------------------------------------------------------
+  // getCats() {
+  //   this.data = this.petService.getAllCats();
+  //   // alert("cattttttt");
+  //   return this.petService.getAllCats();
+  // }
+  // getDogs() {
+  //   this.data = this.petService.getAllDogs();
+  //   return this.petService.getAllDogs();
+  // }
+  // getBirds() {
+  //   this.data = this.petService.getAllBirds();
+  //   return this.petService.getAllBirds();
+  // }
+ 
+  // getAll() {
+  //   this.data = this.petService.getAll();
+  //   return this.petService.getAll();
+  // }
+//--------------------------------------------
+
+
+
+
+
   // getId(range:number[]){
   //   for(let i=0;i<range.length;i++){
   //     return this.data.find((p) => p.id === i);

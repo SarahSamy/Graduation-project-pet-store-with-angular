6b91852eb1890486f7829edc232728cff172107a
fatima shabana <fatima@gmail.com>
2019-06-25 22:17:45 +0200

    'rev'

--- src/app/_services/type.service.ts
+++ src/app/_services/type.service.ts
@@ -1,24 +1,47 @@
-import { Type } from '../_model/Type';
-
+import { Type } from "../_model/Type";
 
 export class TypeService {
-	types: Type[];
-	
+  types: Type[];
 
-	constructor() {
-		this.types = [
-			{ id: 1, name: "Cat",motivationText:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",image:"../../../../assets/images/discover/kitten-jumping-landing-on-feet.jpg" },
-			{ id: 2, name: "Dog" ,motivationText:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",image:"../../../../assets/images/discover/download.jpg" },
-			{ id: 3, name: "Bird",motivationText:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",image:"../../../../assets/images/discover/Conure-Image.jpg"},
-			{ id: 5, name: "Others",motivationText:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",image:"../../../../assets/images/discover/DSC00723JPG_crop350x250.jpg" }
-		]
-	}
-	
-	getAllTypes(): Type[] {
-		return this.types;
-	}
-	// getAllCats(): Type[]{
-		
-	// 	return this.types.filter(t=>t.name=="Cat")
-	// }
+  constructor() {
+    this.types = [
+      {
+        id: 1,
+        name: "Cat",
+        motivationText:
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",
+        image:
+          "../../../../assets/images/discover/kitten-jumping-landing-on-feet.jpg"
+      },
+      {
+        id: 2,
+        name: "Dog",
+        motivationText:
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",
+        image: "../../../../assets/images/discover/download.jpg"
+      },
+      {
+        id: 3,
+        name: "Bird",
+        motivationText:
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",
+        image: "../../../../assets/images/discover/Conure-Image.jpg"
+      },
+      {
+        id: 5,
+        name: "Others",
+        motivationText:
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ab quis consequatur? Architecto obcaecati expedita incidunt autem? Numquam tempore necessitatibus.",
+        image: "../../../../assets/images/discover/DSC00723JPG_crop350x250.jpg"
+      }
+    ];
+  }
+
+  getAllTypes(): Type[] {
+    return this.types;
+  }
+  // getAllCats(): Type[]{
+
+  // 	return this.types.filter(t=>t.name=="Cat")
+  // }
 }

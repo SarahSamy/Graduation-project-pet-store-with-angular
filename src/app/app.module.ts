56c61a47f3f0c46c39f1b317b400838be030df84
esraanew <esraasamir225@gmail.com>
2019-06-25 23:24:00 -0700

    About Us Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,55 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-//import { FormsModule } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule,FormsModule } from "@angular/forms";
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { NgxPaginationModule } from "ngx-pagination";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { FooterComponent } from "./core/layout/footer/footer.component";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { PetItemComponent } from "./features/pets/pet-item/pet-item.component";
-import { PetListingComponent } from "./features/pets/pet-listing/pet-listing.component";
-import { LoginComponent } from "./features/login/login.component";
-import { LowerHeaderComponent } from "./core/layout/header/lower-header/lower-header.component";
-import { UpperHeaderComponent } from "./core/layout/header/upper-header/upper-header.component";
-import { FullHeaderComponent } from "./core/layout/header/full-header/full-header.component";
-import { RegisterComponent } from "./features/register/register.component";
-import { HomeComponent } from "./features/home/home.component";
-import { PetService } from "./_services/pet.service";
-import { FiltringListComponent } from "./shared/filtring-list/filtring-list.component";
-import { PaginationComponent } from "./shared/pagination/pagination.component";
-import { AnimalTypesComponent } from "./features/animal-types/animal-types.component";
-import { AddAnimalFormComponent } from "./features/pets/add-animal-form/add-animal-form.component";
-import { TypeService } from "./_services/type.service";
+
+import { CommentService } from "./_services/Comment.service";
+import { PostsService } from "./_services/Posts.service";
+import { TagService } from "./_services/Tag.service";
 import { CategoryService } from "./_services/category.service";
+import { PetService } from "./_services/pet.service";
+import { ReplyService } from "./_services/reply.service";
+import { TypeService } from "./_services/type.service";
+import { AboutUsComponent } from './features/about-us/about-us.component';
+import { AnimalTypesComponent } from "./features/animal-types/animal-types.component";
+import { HomeComponent } from "./features/home/home.component";
+import { LoginComponent } from "./features/login/login.component";
+import { RegisterComponent } from "./features/register/register.component";
+import { CardWithCoverComponent } from "./shared/card-with-cover/card-with-cover.component";
 import { DropdownComponent } from "./shared/dropdown/dropdown.component";
-import { MainSliderComponent } from "./shared/main-slider/main-slider.component";
+import { FiltringListComponent } from "./shared/filtring-list/filtring-list.component";
 import { FourItemsSliderComponent } from "./shared/four-items-slider/four-items-slider.component";
-import { PetProfileComponent } from "./features/pets/pet-profile/pet-profile.component";
-import { CategoryItemComponent } from "./features/blog/categories/category-item/category-item.component";
-import { CategoryListingComponent } from "./features/blog/categories/category-listing/category-listing.component";
-import { TagListingComponent } from "./features/blog/tags/tag-listing/tag-listing.component";
-import { TagItemComponent } from "./features/blog/tags/tag-item/tag-item.component";
+import { MainSliderComponent } from "./shared/main-slider/main-slider.component";
+import { PaginationComponent } from "./shared/pagination/pagination.component";
+import { FooterComponent } from "./core/layout/footer/footer.component";
+import { NotFoundComponent } from "./core/layout/not-found/not-found.component";
+import { AddPostComponent } from "./features/blog/add-post/add-post.component";
+import { CommentItemComponent } from "./features/blog/comment-item/comment-item.component";
+import { PostDetailsComponent } from "./features/blog/post-details/post-details.component";
 import { PostItemComponent } from "./features/blog/post-item/post-item.component";
 import { PostListingComponent } from "./features/blog/post-listing/post-listing.component";
-import { SidebarComponent } from "./features/blog/sidebar/sidebar.component";
-import { PostsService } from "./_services/Posts.service";
-import { PostDetailsComponent } from "./features/blog/post-details/post-details.component";
-import { AddPostComponent } from "./features/blog/add-post/add-post.component";
-import { TagService } from "./_services/Tag.service";
-import { NgxPaginationModule } from "ngx-pagination";
-import { NotFoundComponent } from "./core/layout/not-found/not-found.component";
-import { ManagePetsComponent } from "./features/user/manage-pets/manage-pets.component";
-import { CommentService } from "./_services/Comment.service";
-import { CommentItemComponent } from "./features/blog/comment-item/comment-item.component";
-import { EditProfileComponent } from "./features/user/edit-profile/edit-profile.component";
-import { CardWithCoverComponent } from "./shared/card-with-cover/card-with-cover.component";
 import { ReplyItemComponent } from "./features/blog/reply-item/reply-item.component";
-import { ReplyService } from "./_services/reply.service";
+import { SidebarComponent } from "./features/blog/sidebar/sidebar.component";
+import { AddAnimalFormComponent } from "./features/pets/add-animal-form/add-animal-form.component";
+import { PetItemComponent } from "./features/pets/pet-item/pet-item.component";
+import { PetListingComponent } from "./features/pets/pet-listing/pet-listing.component";
+import { PetProfileComponent } from "./features/pets/pet-profile/pet-profile.component";
+import { EditProfileComponent } from "./features/user/edit-profile/edit-profile.component";
+import { ManagePetsComponent } from "./features/user/manage-pets/manage-pets.component";
+import { FullHeaderComponent } from "./core/layout/header/full-header/full-header.component";
+import { LowerHeaderComponent } from "./core/layout/header/lower-header/lower-header.component";
+import { UpperHeaderComponent } from "./core/layout/header/upper-header/upper-header.component";
+import { CategoryItemComponent } from "./features/blog/categories/category-item/category-item.component";
+import { CategoryListingComponent } from "./features/blog/categories/category-listing/category-listing.component";
+import { TagItemComponent } from "./features/blog/tags/tag-item/tag-item.component";
+import { TagListingComponent } from "./features/blog/tags/tag-listing/tag-listing.component";
 
 @NgModule({
   declarations: [
@@ -82,12 +86,12 @@ import { ReplyService } from "./_services/reply.service";
     CommentItemComponent,
     EditProfileComponent,
     CardWithCoverComponent,
-    ReplyItemComponent
+    ReplyItemComponent,
+    AboutUsComponent
   ],
   imports: [
     ReactiveFormsModule,
     BrowserModule,
-    // FormsModule,
     NgxPaginationModule,
     AppRoutingModule,
     NgbModule.forRoot(),
@@ -120,6 +124,7 @@ import { ReplyService } from "./_services/reply.service";
       { path: "Blog", component: PostListingComponent },
       { path: "Blog/Post-details/:id", component: PostDetailsComponent },
       { path: "Blog/add-post", component: AddPostComponent },
+      { path: "About_Us", component: AboutUsComponent },
 
       {
         path: "**",
--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators ,FormBuilder} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { User } from 'src/app/_model/user';
+import { UserService } from 'src/app/_services/user.service.service';
+
+import {PasswordValidator} from './register.form.validation';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +14,49 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
-  constructor() { }
+  submitted = false;
+  genders=['Male','Female'];
+  newUser:User;
+
+  constructor(private formBuilder:FormBuilder, private router:Router, private insertData:UserService) {
+    this.newUser={ id: this.insertData.data.length, firstName: "", lastName: "", email: "", password: "", userGroupId: 2, image: "../../../../assets/images/comments/John-Amaechi-OBE_portraitfeatured.jpg" }
+
+   }
 
   ngOnInit() {
+    this.registerForm=this.formBuilder.group({
+      firstName:[null, Validators.required],
+      lastName:[null, Validators.required],
+      email:[null, [Validators.required,Validators.email]],
+      birthDate:[null],
+      gender:['Male'],  
+      password:['', [Validators.required, Validators.minLength(6)] ],
+      confirmPassword:['', [Validators.required]]},
+      {
+        validator:PasswordValidator('password','confirmPassword')
+
+      });
+
   }
 
+  onSubmit(){
+     this.submitted = true;
+   
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.newUser.firstName=this.registerForm.get('firstName').value;    
+    this.newUser.lastName=this.registerForm.get('lastName').value;
+    this.newUser.email=this.registerForm.get('email').value;
+    this.newUser.password=this.registerForm.get('password').value;
+    this.insertData.addPet(this.newUser);
+  // console.log( this.insertData.data)
+ this.router.navigate(['/Login'])
+
+  }
+  Cancel(){
+    return 0;
+  }
 }
+
+

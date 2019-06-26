c787ad8d7900a493f1c511a36350f967e8267851
fatima shabana <fatima@gmail.com>
2019-06-26 02:55:27 +0200

    add footer links and not found selection

--- src/app/_model/user.ts
+++ src/app/_model/user.ts
@@ -1,17 +1,17 @@
-import { Pet } from './Pet';
-import { userGroup } from './userGroup';
+import { Pet } from "./Pet";
+import { userGroup } from "./userGroup";
 
-export interface User{
-    id?: number ;
-    firstName?: string ;
-    lastName?: string ;
-    email:string;
-    password:string;
-    birthDate?:string;
-    gender?: string;
-    // pets?:Pet[];
-    userGroup?:userGroup;
-    userGroupId:number;
-    image?:string;
-    
+export interface User {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  password: string;
+  birthDate?: string;
+  gender?: string;
+  // pets?:Pet[];
+  userGroup?: userGroup;
+  userGroupId: number;
+  image?: string;
+  cover?: string;
 }
